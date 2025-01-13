ee3e34bc0cadbbd49fcf8e629c215bb5c6a10431
Emil Andersson <emil.andersson@cgi.com>
2025-01-13 14:24:51 +0200

    Orgroam.nvim

--- init.lua
+++ init.lua
@@ -967,6 +967,25 @@ require('lazy').setup({
     end,
   },
   {
+    'chipsenkbeil/org-roam.nvim',
+    tag = '0.1.1',
+    dependencies = {
+      {
+        'nvim-orgmode/orgmode',
+        tag = '0.3.7',
+      },
+    },
+    config = function()
+      require('org-roam').setup {
+        directory = '~/org_roam_files',
+        -- optional
+        org_files = {
+          '~/orgfiles',
+        },
+      }
+    end,
+  },
+  {
     'alexghergh/nvim-tmux-navigation',
     config = function()
       local nvim_tmux_nav = require 'nvim-tmux-navigation'
